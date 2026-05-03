6084dd204fe875ecd7bd1488863409c12800eeff
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2026-05-03 13:27:40 +0000

    Update heights

--- src/main.hpp
+++ src/main.hpp
@@ -6,6 +6,7 @@
 #include <optional>
 #include <cassert>
 #include <iostream>
+#include <algorithm>
 
 template <typename Key, typename Value>
 class node {
@@ -21,6 +22,8 @@ public:
 
     node(Key k, Value v);
 
+    void update_heights();
+
     void check_invariants();
 };
 
@@ -32,6 +35,24 @@ node<Key, Value>::node(Key k, Value v) {
 }
 
 template <typename Key, typename Value>
+void node<Key, Value>::update_heights() {
+    std::weak_ptr<node<Key, Value>> current = parent;
+    while (!current.expired()) {
+        std::shared_ptr<node<Key,Value>> current_locked = current.lock();
+        current_locked->height = 0;
+        if (current_locked->left) {
+            current_locked->height = current_locked->left->height + 1;
+        }
+        if (current_locked->right) {
+            current_locked->height =
+                std::max(current_locked->height, current_locked->right->height + 1);
+        }
+
+        current = current_locked->parent;
+    }
+}
+
+template <typename Key, typename Value>
 void node<Key, Value>::check_invariants() {
     if (left) {
         assert(left->parent.lock().get() == this);
@@ -40,6 +61,18 @@ void node<Key, Value>::check_invariants() {
     if (right) {
         assert(right->parent.lock().get() == this);
         right->check_invariants();
+    }
+
+    if (left && right) {
+        // std::cout << "key: " << key << std::endl;
+        // std::cout << "left: " << left->key << std::endl;
+        // std::cout << "right: " << right->key << std::endl;
+        // std::cout << "height: " << height << std::endl;
+        // std::cout << "left height: " << left->height << std::endl;
+        // std::cout << "right height: " << right->height << std::endl;
+        // Doesn't pass yet due to no balancing
+        // assert((height == left->height + 1) || (height == left->height + 2));
+        // assert((height == right->height + 1) || (height == right->height + 2));
     }
 }
 
@@ -59,7 +92,7 @@ std::optional<Value> avl_map<Key, Value>::find(Key key) const {
     auto current = root;
 
     while (current) {
-        auto res = current->key <=> key;
+        auto res = key <=> current->key;
 
         if (res == std::weak_ordering::less) {
             current = current->left;
@@ -83,7 +116,7 @@ void avl_map<Key, Value>::insert(Key key, Value value) {
     auto current = root;
 
     while (current) {
-        auto res = current->key <=> key;
+        auto res = key <=> current->key;
 
         if (res == std::weak_ordering::less) {
             if (current->left) {
@@ -91,6 +124,8 @@ void avl_map<Key, Value>::insert(Key key, Value value) {
             } else {
                 current->left = std::make_shared<node<Key, Value>>(key, value);
                 current->left->parent = current;
+                current->left->update_heights();
+
                 return;
             }
         } else if (res == std::weak_ordering::greater) {
@@ -99,6 +134,8 @@ void avl_map<Key, Value>::insert(Key key, Value value) {
             } else {
                 current->right = std::make_shared<node<Key, Value>>(key, value);
                 current->right->parent = current;
+                current->right->update_heights();
+
                 return;
             }
         } else {
